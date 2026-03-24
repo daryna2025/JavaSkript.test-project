953f7be3e926c22858ece7a6420a46fa1b4f3db0
Daryna Sorokina <daryna.sorokina@student.careeria.fi>
2026-03-24 13:31:44 +0200

    PORT updated

--- server.js
+++ server.js
@@ -2,8 +2,9 @@
 const path = require('path');
 const express = require('express');
 
-
 const app = express();
+
+const PORT = process.env.PORT || 3000;
 
 const consultants = require('./consultants.json');
 
@@ -12,6 +13,10 @@ const polkku = path.join(__dirname, 'public');
 
 app.use(express.static(polkku));
 
-app.listen(3000, () => {
-    console.log('Server is running on port 3000');
+app.listen(PORT, () => {
+  console.log(`server started on port ${PORT}`);
 });
+
+// app.listen(3000, () => {
+//     console.log('Server is running on port 3000');
+// });
